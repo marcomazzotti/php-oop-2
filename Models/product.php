f84de5befa5e4200e0a7a2ee5006a2d3384e7bba
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-05-17 17:31:48 +0200

    Project Completed

--- Models/product.php
+++ Models/product.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once __DIR__ . "/category.php";
 class product
 {
   public $img;
@@ -15,5 +15,16 @@ class product
     $this->description = $_description;
     $this->price = $_price;
     $this->category = $_category;
+  }
+
+  public function printInfo()
+  {
+    $print = "<h4> {$this->name} </h4>";
+    $print .= '<img src="' . $this->img . '" alt="' . $this->name . '">';
+    $print .= "<h6> {$this->description} </h6>";
+    $print .= "<h5> {$this->price} </h5>";
+    $print .= "<p> {$this->category->name} </p>";
+
+    return $print;
   }
 }
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . "/Models/product.php";
 require_once __DIR__ . "/Models/category.php";
+require_once __DIR__ . "/data.php";
 ?>
 
 <!DOCTYPE html>
@@ -11,12 +12,18 @@ require_once __DIR__ . "/Models/category.php";
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style/style.css">
   <title>PetShop</title>
 </head>
 
 <body>
   <div class="container">
     <h1 class="text-center my-4">Pet Shop Online</h1>
+    <?php foreach ($products as $product) { ?>
+      <div class="product my-5 text-center">
+        <?php echo ($product->printInfo()) ?>
+      </div>
+    <?php } ?>
   </div>
 </body>
 
